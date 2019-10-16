5309a169e273a4cacd0f381d12985b018e685e4b
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2019-10-16 17:51:49 +0200

    Update Calculator.java
    program recognizes math operation, but still can't recognize numbers

--- pl/homework/z1/Calculator.java
+++ pl/homework/z1/Calculator.java
@@ -1,45 +1,48 @@
 package pl.homework.z1;
 
-//public class add
-//public class subtract
-//public class multiply
-//public class divide
-
 import java.util.Scanner;
+//static double add (double a){
+//return ()}
+
+//static double subtract {
+//}
+
+//static double multiply {
+//}
+
+//static double divide {
+//}
 
 public class Calculator {
 
-    public static void main(String[] args) {
-        System.out.println("Witaj. Kalkulator");
-        Scanner scan = new Scanner(System.in);
-        while (true) {
-            System.out.println("Wpisz dzialanie:");
-            String expr = scan.nextLine();
-            //System.out.println(expr);
-            if (expr.contains("koniec")) {
-                break;
-            }
-            //here we will parse the variable into three subvariables
-            //double a = Double.parseDouble(expr[0]);
-            //string mark = String.parseString (expr[1]);
-            //double b = Double.parseDouble(expr[2]);
-            //switch (mathExpr){
-            //case "+" :
-            //run add method;
-            //break;
-            //case "-" :
-            //run subtract method;
-            //break;
-            //case "*" :
-            //run multiply method;
-            //break;
-            //case "/" :
-            //run divide method;
-            //break
-            // default:
-            // System.out.println ("Wpisz poprawne działanie ! (+-*/) ");
-            //}
-            //System.out.println ("Wynik" + expr + "= "+ result);
-        }
-    }
-}
+
+	public static void main(String[] args) {
+		System.out.println("Witaj. Kalkulator");
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+			System.out.println("Wpisz dzialanie:");
+			System.out.println("(>koniec< aby zakonczyc)");
+			String expr = scan.nextLine();
+			// System.out.println(expr);
+			if (expr.contains("koniec")) {
+				System.out.println("Koniec programu");
+				break;
+			}
+			if (expr.contains ("+")){
+				System.out.println("dodawanie");
+				
+			} else if (expr.contains("-")) {
+				System.out.println("odejmowanie");
+				
+			} else if (expr.contains("*")) {
+				System.out.println("mnozenie");
+				
+			}else if (expr.contains("/")) {
+				System.out.println("dzielenie");
+			} else System.out.println("Błąd: Nieprawidłowe działanie");
+						}
+			// }
+			// System.out.println ("Wynik" + expr + "= "+ result);
+		}
+		;
+	}
