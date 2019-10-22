cc92a3e0ca5b56f23f79f69e2e1b7e1746635477
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2019-10-22 18:17:34 +0200

    Create MathRiddle.java

---
+++ pl/homework/MathRiddle.java
@@ -0,0 +1,32 @@
+package homework1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MathRiddle {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Random ran = new Random();
+		int x = ran.nextInt(10);
+		int y = ran.nextInt(10);
+		x++;
+		y++; // we have to make them 1-10
+		System.out.println("Pierwsza wylosowana liczba to:" + x);
+		System.out.println("Druga wylosowana liczba to: " + y);
+		int result = x * y;
+		int guess = 0;
+		int counter = 0;
+		System.out.println("Jaki jest iloczyn tych liczb?");
+		do {
+			System.out.println("Wpisz odpowiedz:");
+			guess = scan.nextInt();
+			counter++;
+			if (guess != result)
+			{System.out.println("Błędna odpowiedz!");}
+		} while (result != guess);
+		System.out.println("Brawo, zgadłeś w " + counter+ " próbie!" );
+
+	}
+
+}
