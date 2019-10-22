d3e8e8dae35fb38070cd7ba5bfbf237c293b954c
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2019-10-22 18:27:59 +0200

    Create GuessTheNumber.java

---
+++ pl/homework/GuessTheNumber.java
@@ -0,0 +1,30 @@
+package homework1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumber {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Random ran = new Random();
+		int x = ran.nextInt(1000);
+		System.out.println("Wylosowałem liczbę od 0 do 999, spróbuj zgadnąć");
+		int guess = 0;
+		int counter = 0;
+		do {
+			System.out.println("Wpisz odpowiedz:");
+			guess = scan.nextInt();
+			counter++;
+			if (guess < x) {
+				System.out.println("Twoja liczba jest za mała");
+			}
+			if (guess > x) {
+				System.out.println("Twoja liczba jest za duża");
+			}
+		} while (x != guess);
+		System.out.println("Brawo, zgadłeś w " + counter + " próbie!");
+		scan.close();
+	}
+
+}
