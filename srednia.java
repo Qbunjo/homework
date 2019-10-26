5bd2f1ca44464e4ee9a2e82ad54643e6d1287033
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2019-10-26 12:50:13 +0200

    Create srednia.java

---
+++ srednia.java
@@ -0,0 +1,26 @@
+package tablice;
+
+import java.util.Scanner;
+
+public class Srednia15 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Policzymy srednia:");
+		System.out.println("Ile liczb chcesz wpisac?");
+		int Tleng = scan.nextInt();
+		int[] arr = new int[Tleng];
+		for (int i = 0; i < Tleng; i++) {
+			System.out.println("Wpisz liczbę nr " + (i + 1));
+			arr[i] = scan.nextInt();
+		}
+		System.out.println("Liczymy średnią");
+		double sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+		}
+		sum /= arr.length;
+		System.out.println("Srednia tych liczb: " + sum);
+	}
+
+}
