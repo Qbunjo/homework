13d413a9cf52f5b23dc2570371705f2a3ad1e1da
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2019-10-26 15:45:25 +0200

    Create silnia3.java

---
+++ silnia3.java
@@ -0,0 +1,27 @@
+package funkcje;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class silnia3 {
+
+	static BigInteger silnia(int n) {
+		BigInteger result = BigInteger.ONE;
+		for (int i = 1; i <= n; i++) {
+
+			result = result.multiply(BigInteger.valueOf(i));
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Podaj argument:");
+		int n = sc.nextInt();
+
+		BigInteger wynik = silnia(n);
+		System.out.println("Wynik: " + wynik);
+		sc.close();
+	}
+
+}
