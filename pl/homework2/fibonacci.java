09ec57f754f99f5b8d372b6ce2d07e48ba118915
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2019-10-29 21:08:21 +0100

    Add files via upload

---
+++ pl/homework2/fibonacci.java
@@ -0,0 +1,16 @@
+package homework2;
+
+public class fibonacci {
+	
+	static long fiNumber(int cyfra) {
+		//String policz = String.valueOf(wybor);
+		//int p= policz.length();
+		return p;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ pl/homework2/sumaNieparzystych.java
@@ -0,0 +1,24 @@
+package homework2;
+
+public class sumaNieparzystych {
+
+	static int sumNiepar(int[] t) {
+		int suma = 0;
+
+		for (int element : t) {
+			if (element % 2 != 0) {
+				suma += element;
+			}
+		}
+
+		return suma;
+	}
+
+	public static void main(String[] args) {
+
+		int[] liczby = { 13, 12, 15, 18, 19, 15, 13, 23, 56, 32, 23, 21, 3 };
+
+		System.out.println("suma nieparzystych " + sumNiepar(liczby));
+	}
+
+}
