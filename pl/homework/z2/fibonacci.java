f28ad291f141c256dad1ad329ec5221f010157ee
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2019-10-31 09:08:36 +0100

    Update fibonacci.java

--- pl/homework/z2/fibonacci.java
+++ pl/homework/z2/fibonacci.java
@@ -1,22 +1,34 @@
-package pl.homework.z2;
+//package pl.homework.z2;
+
+import java.util.Scanner;
 
 public class Fibonacci {
-	
-	static long FiNumber(int cyfra) {
-		if (cyfra ==0) {
-			return 0};
-		if (cyfra ==1 {
-			return 1};
-		    for (int i; i==cyfra,i++){
-			    
-		return p;}
-	}
 
-	public static void main(String[] args) {
-		sout ("Program oblicza kolejną liczbę ciągu Fibonacciego");
-		sout ("Którą liczbę ciągu chcesz obliczyć?:")
-			Scanner sc = new Scanner;
+    static long FiNumber(int cyfra)
+    {
+        if (cyfra == 0) {
+            return 0;
+        }
+        if (cyfra == 1) {
+        return 1;
+    }// pierwszym argumentem, od którego liczymy jest 2, a 1+2=3, toteż p=3
+        long p=3;
+        for (int i=2; i == cyfra; i++){
+        p += (i+1);
+            System.out.println(p);
 
-	}
-
+    }
+        return p;
+    }
 }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Program oblicza kolejną liczbę ciągu Fibonacciego");
+        System.out.println("Którą liczbę ciągu chcesz obliczyć?:");
+
+        int InpNum = sc.nextInt();
+        FiNumber(InpNum);
+
+
+    }
