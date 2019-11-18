a1340129f869f5f2f640eeed955b800ad04363b3
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2019-11-18 12:52:51 +0100

    Create TestOgloszenie.java

---
+++ pl/homework/meeting03/TestOgloszenie.java
@@ -0,0 +1,17 @@
+package ogloszenie;
+
+public class TestOgloszenie {
+
+	public static void main(String[] args) {
+		OgloszenieSamochodowe oglo1=new OgloszenieSamochodowe("Nówka Fiacik", null, 0, null, null, null, 0, 0, 0, 0, null, false);
+		OgloszenieSamochodowe oglo2=new OgloszenieSamochodowe("Sprzedam Opla", null, 0, null, null, null, 0, 0, 0, 0, null, false);
+		//OgloszenieNieruchomosci ogln1= new OgloszenieNieruchomosci(null, 0, 0);
+		
+		String druk=oglo1.toString();
+		System.out.println(druk);
+		druk=oglo2.toString();
+		System.out.println(druk);
+
+	}
+
+}
