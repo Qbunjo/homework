2d1be50e48205d89360304fd33d65ea5d6fb2693
qbunjo <jakub@jakalski.pl>
2019-11-25 14:12:57 +0100

    init version

---
+++ src/pl/homework/meeting03/e02/Main.java
@@ -0,0 +1,22 @@
+package pl.homework.meeting03.e02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<FuelStation> stations = new ArrayList <>(); 
+				FuelStation station1= new FuelStation("StatOil", 3.40,2.50,3.20);
+				FuelStation station2= new FuelStation("Stacja BP", 3.30,3.2,4.3);
+				FuelStation station3=new FuelStation("Orlen", 2.30,4.30,3.40);
+				FuelStation station4= new FuelStation("Stacja Ziutka", 2.40,3.34,5.43);
+		 stations.add(station1);
+		 stations.add(station2);
+		 stations.add(station3);
+		 stations.add(station4);
+		 
+
+}
+		}
+
