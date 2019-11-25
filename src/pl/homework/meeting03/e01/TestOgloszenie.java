8e91d5cd59a7f479f54de632407cfcf488b5143e
qbunjo <jakub@jakalski.pl>
2019-11-25 12:42:17 +0100

    last remarks

--- src/pl/homework/meeting03/e01/TestOgloszenie.java
+++ src/pl/homework/meeting03/e01/TestOgloszenie.java
@@ -3,13 +3,14 @@ package pl.homework.meeting03.e01;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
 
 public class TestOgloszenie {
 
 	public static void main(String[] args) {
+		String filePath = "C:\\Users\\Jakuboslaw\\eclipse-workspace\\JJ01-Podstawy\\src\\homework1\\homework\\src\\pl\\homework\\meeting03\\e01\\";
 		// String filePath =
-		// "C:\\Users\\Jakuboslaw\\eclipse-workspace\\git\\src\\main\\java\\pl\\homework\\meeting03\\e01\\";
-		String filePath = "/home/qbunjo/git/homework/src/main/java/pl/homework/meeting03/e01/";
+		// "/home/qbunjo/git/homework/src/main/java/pl/homework/meeting03/e01/";
 		// linux path (cause I work on both systems)
 		File plik = new File(filePath + "ogloszenia.csv");
 
@@ -32,6 +33,23 @@ public class TestOgloszenie {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		System.out.println("All announcements in my base:");
+		for (Ogloszenie oglo : list2) {
+			System.out.println(oglo);
+		}
+		// sorting
+		System.out.println("I assume you want to find some cars");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("What is your maximum price?:");
+		int carMax = sc.nextInt();
+		System.out.println("What is your minimum price?:");
+		int carMin = sc.nextInt();
+		System.out.println("sorting");
+		for (Ogloszenie car : list2) {
+			if (car instanceof OgloszenieSamochodowe & car.getCena() < carMax & car.getCena() > carMin) {
+				System.out.println(car);
+			}
+		}
+		sc.close();
 	}
-
 }
