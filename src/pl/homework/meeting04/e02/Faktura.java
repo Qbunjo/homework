6a2f30d3ee5e3088e9198f8d12e45def9486e095
qbunjo <jakub@jakalski.pl>
2019-12-14 12:13:21 +0100

    subclass

--- src/pl/homework/meeting04/e02/Faktura.java
+++ src/pl/homework/meeting04/e02/Faktura.java
@@ -4,40 +4,86 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Faktura {
+	private List lista;
 	private String numer;
 	private String towar;
 	private int ilosc;
 	private Double cena;
 	private int vat;
-	
-	public List<Faktura> lista = new ArrayList<>();
 
-	public Faktura(String numer, String towar, int ilosc, Double cena, int vat) {
-		
+	public Faktura(List lista) {
+		this.lista = lista;
+
 	}
-	public void dodajPozycje(String towar, int ilosc, double cena, int vat) {
-		Faktura pozycja = new Faktura(numer,towar, ilosc, cena,vat);
-		lista.add(numer,towar,ilosc,cena,vat);
+
+	public class Pozycja {
+		public Pozycja(String numer2, String towar2, int ilosc2, Double cena2, int vat2) {
+			// TODO Auto-generated constructor stub
+		}
+		private String numer;
+		private String towar;
+		private int ilosc;
+		private Double cena;
+		private int vat;
+	}
+
+	public void  Pozycja(String numer, String towar, int ilosc, Double cena, int vat) {
+		this.numer=numer;
+		this.towar=towar;
+		this.ilosc=ilosc;
+		this.cena=cena;
+		this.vat=vat;
+	}
+
+	public void dodajPozycje(String numer, String towar, int ilosc, Double cena, int vat) {
+		Pozycja pozycja = new Pozycja(numer,towar,ilosc,cena,vat);
+		lista.add(pozycja);
 	}
 
 	public int iloscPozycji(String numer) {
-		
-		int ilosc=lista.size();;
+
+		int ilosc = lista.size();
+		;
 		return ilosc;
 	}
 
 	public double wartoscPozycji(String numer, int numerPozycji) {
-		Double wartosc = cena*vat*ilosc;
+		Double wartosc = cena * vat * ilosc;
 		return wartosc;
 	}
+
 	public double doZaplaty() {
 		return 0;
-		
+
 	};
-	public void wydrukuj(){
-		for (int i=0; i==lista.size();i++) {
-			System.out.println("drukuj wszystko");	
+
+	public void wydrukuj() {
+		for (int i = 0; i == lista.size(); i++) {
+			System.out.println("drukuj wszystko");
 		}
-		
+
 	}
+
+	public String getNumer() {
+		return numer;
+	}
+
+	public String getTowar() {
+		return towar;
+	}
+
+	public int getIlosc() {
+		return ilosc;
+	}
+
+	public Double getCena() {
+		return cena;
+	}
+
+	public int getVat() {
+		return vat;
+	}
+
+	
+
 }
--- src/pl/homework/meeting04/e02/FakturaTest.java
+++ src/pl/homework/meeting04/e02/FakturaTest.java
@@ -4,15 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import pl.homework.meeting04.e02.Faktura.Pozycja;
+
 class FakturaTest {
 
 		
 	@Test void Faktura() {
-		
+		Faktura nowa=new Faktura(null);
+		Faktura.Pozycja("1","Lodówka",2,233.3,30);
+		System.out.println(nowa.getTowar());
+	assertEquals("Lodowka", nowa.getTowar());
 	}
 	
 	@Test void dodajPozycje() {
-	
+		
 	}
 	}
 
